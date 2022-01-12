896333f05e5a1f8d68e13c4292ed702245175ffe
amrrs <1littlecoder@gmail.com>
2022-01-13 01:48:02 +0530

    with some minor changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,30 @@
-import streamlit as st
-import nltk
-from nltk.corpus import words
-import re
+import streamlit as st # this is for web app
+import nltk # nltk for english words
+from nltk.corpus import words # nltk words is to get five letter words
 
-st.title("Wordle ResCLUEr")
+st.title("Wordle ResCLUEr") # this is the title of the app
 
-@st.cache
+# this is to display the image of wordle game
+st.image("https://www.dexerto.com/wp-content/uploads/2022/01/08/wordle-tweets.jpg.webp")
+
+# download the english words dictionary 
+
+@st.cache # cache the download process 
 def download():
     nltk.download('words')
 download()
 
+
+# create a new list with only five letter words
+
 five_letters = [word for word in words.words() if len(word)==5 ]
 
+# create a five column grid
+
 [a,b,c,d,e] = st.columns(5)
+
+# get user input currently doesnt handle number of characters
+
 with a:
     first_letter = st.text_input(label="1st",value = 'a')
 with b:
@@ -22,7 +34,9 @@ with c:
 with d:
     fourth_letter = st.text_input(label="4th", value = '')
 with e:
-    fifth_letter = st.text_input(label="5tth", value = 't')
+    fifth_letter = st.text_input(label="5th", value = 't')
+
+# combine all the letters 
 
 clue = first_letter+second_letter+third_letter+fourth_letter+fifth_letter
 
@@ -30,17 +44,25 @@ st.markdown("### clue")
 
 st.write(clue)
 
-st.markdown("# Exclusion letters")
+st.markdown("### Exclusion letters")
+
+# exclusion letters where the grey grids are 
 
 exclusions = st.text_input(label="exclusions")
 
 st.markdown("# Wordle Clues")
 
+
+# this is an empty list to show all the clue words
+
 clue_result = []
+
 
 for word in five_letters:
  if all(c in word for c in clue) and not any(c in word for c in exclusions):
    clue_result.append(word)
 
-st.write(clue_result)
+# print the output list of clues
+
+st.write(clue_result) 
   
